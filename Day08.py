3a204dc37b9d3a308255022e825aaf3d40af1850
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2020-04-23 12:12:29 +0200

    Finished day 8

--- Day08.py
+++ Day08.py
@@ -15,7 +15,7 @@ data = []
 with(open("Data - Day08.txt")) as file:
     for line in file:
         data.append(line)
-data = pd.DataFrame(data)
+
 # GOAL 1
 """You come across a door implementing what you can only assume is an
 implementation of two-factor authentication after a long game of requirements
@@ -45,86 +45,33 @@ Pixels that would fall off the bottom appear at the top of the column.
 """
 
 # ANSWER 1
-def find_action(string):
-    """Discovers which action should be taken."""
-    if "rect" in string:
-        action = "rect"
-    elif "rotate row" in string:
-        action = "rotate row"
-    elif "rotate column" in string:
-        action = "rotate column"
-    else:
-        action = "unknown"
-    return action
+array = np.zeros((6, 50), dtype=bool)
 
+for line in data:
+    split_line = re.split(r"[ =]", line)
 
-def find_rect_cordinates(string, return_val):
-    """For all rect actions, discover the exact rectangle to be switched on."""
-    if "rect" in string:
-        coordinate1 = int(re.search(r"(\d)x", string).group(1))
-        coordinate2 = int(re.search(r"x(\d)", string).group(1))
-    else:
-        coordinate1 = np.nan
-        coordinate2 = np.nan
+    if split_line[0] == "rect":
+        width, height = map(int, split_line[1].split('x'))
+        array[:height, :width] = True
 
-    if return_val == 1:
-        return coordinate1
-    elif return_val == 2:
-        return coordinate2
-    else:
-        print("No values are returned")
+    elif split_line[0] == "rotate":
+        if split_line[1] == "row":
+            rotate_loc = int(split_line[3])
+            rotate_by = int(split_line[5])
+            array[rotate_loc] = np.roll(array[rotate_loc], rotate_by)
+        else:
+            rotate_loc = int(split_line[3])
+            rotate_by = int(split_line[5])
+            array[:, rotate_loc] = np.roll(array[:, rotate_loc], rotate_by)
 
-
-def find_rotate_loc(string):
-    """Find the rotate locations."""
-    if "rotate" in string:
-        rotate_loc = int(re.search(r"[xy]=(\d)", string).group(1))
-    else:
-        rotate_loc = np.nan
-
-    return rotate_loc
-
-
-def find_rotate_by(string):
-    """Find how much to rotate."""
-    if "rotate" in string:
-        rotate_by = int(re.search(r"by\s(\d)", string).group(1))
-    else:
-        rotate_by = np.nan
-
-    return rotate_by
-
-
-def switch_pixels_on(array, data, row_idx):
-    """Switch pixels on."""
-    width = int(data["Rect_cor1"].iloc[row_idx])
-    height = int(data["Rect_cor2"].iloc[row_idx])
-
-    array[0:height, 0:width] = 1
-
-    return array
-
-def rotate_pixels(array, data, row_idx, column=True):
-    """Rotate pixels."""
-    rot_loc = int(data["Rotate_location"].iloc[row_idx])
-    rot_by = int(data["Rotate_by"].iloc[row_idx])
-
-    if column:
-        array[:, rot_loc] = np.roll(array[:, rot_loc], rot_by, axis=0)
-    else:
-        array[rot_loc, :] = np.roll(array[rot_loc, :], rot_by, axis=1)
-
-    return array
-
-def perform_action(data, array):
-    pass
-
-data["Action"] = data[0].apply(find_action)
-data["Rect_cor1"] = data[0].apply(find_rect_cordinates, return_val=1)
-data["Rect_cor2"] = data[0].apply(find_rect_cordinates, return_val=2)
-data["Rotate_location"] = data[0].apply(find_rotate_loc)
-data["Rotate_by"] = data[0].apply(find_rotate_by)
-
-array = np.zeros((6, 50))
-array = perform_action(data, array)
 print(f"{np.sum(array)} pixels are lit.")
+
+# GOAL 2
+"""You notice that the screen is only capable of displaying capital letters;
+in the font it uses, each letter is 5 pixels wide and 6 tall.
+
+After you swipe your card, what code is the screen trying to display?"""
+
+# ANSWER 2
+#use variable explorer in IDE
+print("ZFHFSFOGPO")
